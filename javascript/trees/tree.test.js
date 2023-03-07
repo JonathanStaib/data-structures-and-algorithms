be4538f1f4319aa9190137042ca096ebdfb5c9c3
JonathanStaib <JonathanStaibb@gmail.com>
2023-03-07 22:00:57 +0000

    completed code challenge 17

--- javascript/trees/tree.test.js
+++ javascript/trees/tree.test.js
@@ -90,4 +90,18 @@ describe('tree', () => {
 
   });
 
+  it('will push all elements into array as it goes in', () => {
+    let tree = new Tree();
+    tree.root = new Node(10);
+    tree.root.left = new Node(5);
+    tree.root.right = new Node(15);
+    tree.root.left.left = new Node(1);
+    tree.root.left.right = new Node(8);
+    tree.root.right.right = new Node(17);
+
+    const breadthResults = tree.breadthFirst(tree);
+
+    expect(breadthResults).toEqual([10, 5, 15, 1, 8, 17]);
+  });
+
 });
