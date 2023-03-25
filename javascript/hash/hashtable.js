7cf37eb68c05a6c83f4cbb7ab59c12b44adbe81e
JonathanStaib <JonathanStaibb@gmail.com>
2023-03-25 18:49:42 +0000

    what we did in lecture

---
+++ javascript/hash/hashtable.js
@@ -0,0 +1,60 @@
+'use strict';
+
+class HashTable {
+  constructor(size){
+    this.size = size;
+    this.buckets = new Array(size);
+  }
+
+  hash(key){
+    let characters = key.split('');
+
+    let asciiSum = characters.reduce((sum, character) => {
+      return sum + character.charCodeAt(0);
+    }, 0);
+
+    let initialHash = asciiSum * 599;
+    return initialHash % this.size;
+  }
+
+  set(key, value){
+
+    let position = this.hash(key);
+
+    let data = {[key] : value};
+
+    if(this.buckets[position]){
+      let bucket = this.buckets[position];
+      bucket.add(data);
+    } else{
+      let bucket = data;
+    }
+  }
+
+  get(key){
+    let position = this.hash(key);
+
+    if(this.buckets[position]){
+      let bucket = this.buckets[position];
+      return bucket.value;
+    }
+  }
+
+  has(key){
+    let position = this.hash(key);
+
+    return this.buckets[position] === true;
+  }
+}
+
+
+
+const table = new HashTable(1024);
+
+const hashOne = table.hash('Jonny');
+const hashTwo = table.hash('Jill');
+const hashThree = table.hash('Mike');
+
+console.log('hashOne:', hashOne);
+console.log('hashTwo:', hashTwo);
+console.log('hashThree:', hashThree);
