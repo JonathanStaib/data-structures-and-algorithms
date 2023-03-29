54925a5f96edc795fb22a0a1f5b2db6d099daf93
JonathanStaib <JonathanStaibb@gmail.com>
2023-03-29 21:52:51 +0000

    code challenge 33

--- javascript/hash/hashtable.js
+++ javascript/hash/hashtable.js
@@ -25,10 +25,9 @@ class HashTable {
 
     if(this.buckets[position]){
       let bucket = this.buckets[position];
-      bucket.add(data);
+      bucket[`${key}`] = value;
     } else{
-      let bucket = data;
-      this.buckets[position].push(bucket);
+      this.buckets[position] = data;
     }
   }
 
@@ -41,9 +40,9 @@ class HashTable {
     }
   }
 
-  keys(){
-    let keys = this.buckets.map([position]);
-  }
+  // keys(){
+  //   let keys = this.buckets.map([position]);
+  // }
 
   has(key){
     let position = this.hash(key);
@@ -75,6 +74,30 @@ class HashTable {
     }
     return null;
   }
+
+  // leftjoin(hashmap1, hashmap2) {
+
+  //   let position = hashmap1.hash(key)
+  //   let position2 = hashmap2.hash(key)
+  //   let table = new hashTable();
+  //   let k = ''
+
+  //   for (i = 0; i < hashmap1.length; i++) {
+  //     k = position;
+
+  //     for (i = 0; i < hashmap2.length; i++) {
+
+  //       if (position2 === k) {
+  //         table.set(`${k}`, k.value, position2.value);
+  //       }
+
+  //     } if (!(key in hashmap2 === k)) {
+  //       table.set(`${k}`, k.value, null);
+  //     }
+
+  //   } return table;
+  // }
+
 }
 
 
@@ -88,3 +111,5 @@ const hashThree = table.hash('Mike');
 console.log('hashOne:', hashOne);
 console.log('hashTwo:', hashTwo);
 console.log('hashThree:', hashThree);
+
+module.exports = HashTable;
